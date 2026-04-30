a79d0ee3b58c6c18c5631c5df1a1f56c50466945
Sepehr <amini.sep@gmail.com>
2026-04-30 16:21:08 +0200

    git ignore reset

--- android/app/src/main/kotlin/com/sepehramini/keymage/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sepehramini.keymage
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity() {}
--- android/app/src/main/kotlin/com/sepehramini/pwd_gen/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sepehramini.pwd_gen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
